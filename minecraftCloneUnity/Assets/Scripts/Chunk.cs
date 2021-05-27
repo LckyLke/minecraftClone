e38b06de210cfb20f91a62644cb3ecac7e2f952d
LckyLke <lukefriedrichs@gmail.com>
2021-05-27 14:58:29 +0200

    Chunk Render improved! Redundant information for the mesh renderer removed! (Redudant indexes of vertices)
    1. Chunks now only Render their outer surface
    2. The redudant indexes of vertecies in the triangles Array have been removed
    =>
    The Render loop has been removed and code has been changed to use certain vertecies twice to render triangles

--- minecraftCloneUnity/Assets/Scripts/Chunk.cs
+++ minecraftCloneUnity/Assets/Scripts/Chunk.cs
@@ -8,25 +8,108 @@ public class Chunk
     MeshFilter meshFilter;
     GameObject chunkObject;
     Mesh mesh;
+
+    int vertexIndex = 0; //Gibt den aktuellen Vektorplatzt an (im Bezug auf den Chunk context)
+
+    
+
+    List<int> Triangles = new List<int>();
+    List<Vector3> Vertices = new List<Vector3>();
+
+
+    bool[,,] voxelMap = new bool[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];
     
     public Chunk(World _world)
     {
         World world = _world;
+
+        CreateChunkAndItsMesh(world);
+        PopulateVoxelMap();
+
+        GenerateNewChunk(world.transform.position);
+        CreateMesh();
+        
+    }
+
+    void PopulateVoxelMap()
+    {
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                {
+                    voxelMap[x, y, z] = true;
+                }
+            }
+        }
+    }
+
+    bool CheckVoxel (Vector3 pos) //Gibt true wenn ein block solid ist!
+    {
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
+
+        if (x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > VoxelData.ChunkHeight - 1 || z < 0 || z > VoxelData.ChunkWidth - 1)//Überprüft ob es sich um einen Block außerhalb der Begrenzung handelt(nicht existent:0)
+            //Folglich muss dieser nicht solid sein da er nicht existiert
+        {
+            return false;
+        }
+
+        return voxelMap[x,y,z];
+    }
+
+    void CreateChunkAndItsMesh(World world)
+    {
         chunkObject = new GameObject();
         chunkObject.transform.SetParent(world.transform);
         meshRenderer = chunkObject.AddComponent<MeshRenderer>();
         meshFilter = chunkObject.AddComponent<MeshFilter>();
-
-        GenerateChunk();
-        meshFilter.mesh = mesh;
     }
 
-    void GenerateChunk()
+    void GenerateNewVoxel(Vector3 pos)
+    {
+        
+        
+        for (int p = 0; p < 6; p++)
+        {
+            if (!CheckVoxel(pos + VoxelData.faceChecks[p])) //Es wird überprüft ob nachbar Block solid ist und wenn nein wird die entsprechende Seite generiert!
+            {
+                Vertices.Add(pos + VoxelData.VerticesArr[VoxelData.TriangelsArr[p][0]]);
+                Vertices.Add(pos + VoxelData.VerticesArr[VoxelData.TriangelsArr[p][1]]);
+                Vertices.Add(pos + VoxelData.VerticesArr[VoxelData.TriangelsArr[p][2]]);
+                Vertices.Add(pos + VoxelData.VerticesArr[VoxelData.TriangelsArr[p][3]]);
+                Triangles.AddRange(new int[6] { vertexIndex, vertexIndex + 1, vertexIndex + 2, vertexIndex + 2, vertexIndex + 1, vertexIndex + 3 });
+                vertexIndex += 4;
+
+            }
+        }
+
+       
+    }
+
+    void GenerateNewChunk(Vector3 worldPos)
+    {
+        for (int y=0; y < VoxelData.ChunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.ChunkWidth; z++)
+                {
+                    if (voxelMap[x,y,z]) 
+                        GenerateNewVoxel(worldPos + new Vector3(x, y, z));
+                }
+            }
+        } 
+    }
+
+    void CreateMesh()
     {
         mesh = new Mesh();
-        mesh.vertices = VoxelData.VerticesArr;
-
-        mesh.triangles = VoxelData.TriangelsArr[0];
+        mesh.vertices = Vertices.ToArray();
+        mesh.triangles = Triangles.ToArray();
         mesh.RecalculateNormals();
+        meshFilter.mesh = mesh;
     }
 }
--- minecraftCloneUnity/Assets/Scripts/VoxelData.cs
+++ minecraftCloneUnity/Assets/Scripts/VoxelData.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class VoxelData
 {
-    public static readonly int ChunkWidth = 5;
-    public static readonly int ChunkHeigth = 5;
+    public static readonly int ChunkWidth = 15;
+    public static readonly int ChunkHeight = 5;
 
     public static readonly Vector3[] VerticesArr = new Vector3[8]
     {
@@ -14,18 +14,28 @@ public class VoxelData
        new Vector3(1,1,0), //2
        new Vector3(0,1,0), //3
        new Vector3(0,0,1), //4
-       new Vector3(0,0,0), //5
+       new Vector3(1,0,1), //5
        new Vector3(1,1,1), //6
        new Vector3(0,1,1), //7
     };
 
+    public static readonly Vector3[] faceChecks = new Vector3[6]
+    {
+        new Vector3(0,0,-1),
+        new Vector3(0,0,1),
+        new Vector3(0,1,0),
+        new Vector3(0,-1,0),
+        new Vector3(-1,0,0),
+        new Vector3(1,0,0),
+    };
+
     public static readonly int[][] TriangelsArr = new int[6][]
     {
-        new int [6] {0, 3, 1, 1, 3, 2}, //back
-        new int [6] {5, 6, 4, 4, 6, 7}, //front
-        new int [6] {3, 7, 2, 2, 7, 6}, //top
-        new int [6] {1, 5, 0, 0, 5, 4}, //bottom
-        new int [6] {4, 7, 0, 0, 7, 3}, //left
-        new int [6] {1, 2, 5, 5, 2, 6} //right  
+        new int [4] {0, 3, 1, 2}, //back
+        new int [4] {5, 6, 4, 7}, //front
+        new int [4] {3, 7, 2, 6}, //top
+        new int [4] {1, 5, 0, 4}, //bottom
+        new int [4] {4, 7, 0, 3}, //left
+        new int [4] {1, 2, 5, 6} //right  
     };
 }
